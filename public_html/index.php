f83bb993e77d756de842edae86fa2c07573a07de
Sunnefa Lind <sunnefa_lind@hotmail.com>
2012-05-16 10:53:41 +0100

    Changed index.php

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,9 @@ require_once '../config/init.php';
 
 $sql = new MySQLWrapper(MYSQL_USER, MYSQL_USER, MYSQL_USER, MYSQL_HOST);
 
-
+if(isset($_REQUEST['page'])) {
+    
+}
 
 include '../controllers/core/pages.php';
 
